ede0428ee6444aee9c8b671350d9d15256a4cba3
luisEfe80 <luismendez80@hotmail.com>
2021-10-27 14:48:54 -0400

    Adición de introducción

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,10 +9,18 @@
           i.fas.fa-info
       h1 Introducción
     
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5.slideInUp
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t0-i1.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 Este material se considera una guía para los equipos de desarrollo curricular, en la resignificación de los conceptos relativos con la <b>Formación para el Trabajo y el Desarrollo Humano (FTDH)</b>, por lo que se invita a observar la introducción al mismo.
+
     figure.mb-5
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
     
 
 
